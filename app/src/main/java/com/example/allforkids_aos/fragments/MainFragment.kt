59e00e777a6fa703ce4a9bf3fa39a108a136b03c
alschlee <1daysixmeal@gmail.com>
2023-08-06 03:17:37 +0900

    [FEAT] BottomSheet 프래그먼트 전환 구현

---
+++ app/src/main/java/com/example/allforkids_aos/fragments/MainFragment.kt
@@ -0,0 +1,9 @@
+package com.example.allforkids_aos.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.allforkids_aos.R
+
+
+class MainFragment : Fragment(R.layout.fragment_main) {
+
+}
