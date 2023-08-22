22a592f2997be1ce399342a2c7d29dc8c32338fb
kchaeeun <oceanball38@naver.com>
2023-08-22 13:54:00 +0900

    [FEAT] 탈퇴하기 UI #2

--- app/src/main/java/com/example/allforkids_aos/AnnouncementDetailActivity.kt
+++ app/src/main/java/com/example/allforkids_aos/AnnouncementDetailActivity.kt
@@ -7,6 +7,10 @@ class AnnouncementDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+
+
+
+
         setContentView(R.layout.activity_announcement_detail)
     }
 }
