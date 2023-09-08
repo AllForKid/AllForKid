a2fc2b38d952cd6a3e4c3580f222795bc7f24266
kchaeeun <oceanball38@naver.com>
2023-09-08 21:27:17 +0900

    [FEAT] Dialog 기능 구현

--- app/src/main/java/com/example/allforkids_aos/AccountInterworkActivity.kt
+++ app/src/main/java/com/example/allforkids_aos/AccountInterworkActivity.kt
@@ -1,18 +1,25 @@
 package com.example.allforkids_aos
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.WindowManager
 import android.widget.Toast
+import androidx.core.content.ContentProviderCompat
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.allforkids_aos.adapter.HelpAdapter
 import com.example.allforkids_aos.adapter.InterworkAccountAdapter
+import com.example.allforkids_aos.databinding.AccountLinkedDialogBinding
 import com.example.allforkids_aos.databinding.ActivityAccountInterworkBinding
 import com.example.allforkids_aos.model.Help
 import com.example.allforkids_aos.model.InterworkAccount
 
 class AccountInterworkActivity : AppCompatActivity() {
     private lateinit var viewBinding: ActivityAccountInterworkBinding
+    private val accountList = mutableListOf<InterworkAccount>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewBinding = ActivityAccountInterworkBinding.inflate(layoutInflater)
@@ -20,17 +27,17 @@ class AccountInterworkActivity : AppCompatActivity() {
 
         viewBinding.actionbar.appbarAccountInterwork.visibility = View.VISIBLE
 
+        setupRecyclerView()
         viewBinding.tvCopyMyCode.setOnClickListener {
             Toast.makeText(this, "연동 코드를 복사했습니다", Toast.LENGTH_LONG).show()
         }
-        setupRecyclerView()
     }
 
     private fun setupRecyclerView() {
         val accountList = createHelpList()
         val accountRecyclerView = viewBinding.rvAccountLink
         accountRecyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-        accountRecyclerView.adapter = InterworkAccountAdapter(accountList)
+        accountRecyclerView.adapter = InterworkAccountAdapter(this,accountList)
     }
 
     private fun createHelpList(): List<InterworkAccount> {
--- app/src/main/java/com/example/allforkids_aos/adapter/InterworkAccountAdapter.kt
+++ app/src/main/java/com/example/allforkids_aos/adapter/InterworkAccountAdapter.kt
@@ -1,12 +1,24 @@
 package com.example.allforkids_aos.adapter
 
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.view.WindowManager
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.recyclerview.widget.RecyclerView
+import com.example.allforkids_aos.databinding.AccountLinkedDialogBinding
+import com.example.allforkids_aos.databinding.AskDialogBinding
 import com.example.allforkids_aos.databinding.ItemAccountBinding
 import com.example.allforkids_aos.model.InterworkAccount
+import com.example.allforkids_aos.model.MyJjim
 
-class InterworkAccountAdapter(private val accountList: List<InterworkAccount>) : RecyclerView.Adapter<InterworkAccountAdapter.InterworkAccountViewHolder>() {
+class InterworkAccountAdapter(
+    private val context: Context,
+    private val accountList: List<InterworkAccount>) : RecyclerView.Adapter<InterworkAccountAdapter.InterworkAccountViewHolder>() {
+    private var onItemClickListener: ((InterworkAccount) -> Unit)? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): InterworkAccountViewHolder {
         val itemBinding = ItemAccountBinding.inflate(LayoutInflater.from(parent.context),parent, false)
@@ -16,6 +28,10 @@ class InterworkAccountAdapter(private val accountList: List<InterworkAccount>) :
     override fun onBindViewHolder(holder: InterworkAccountViewHolder, position: Int) {
         val account = accountList[position]
         holder.bind(account)
+
+        holder.itemView.setOnClickListener {
+            onItemClickListener?.invoke(account)
+        }
     }
 
     override fun getItemCount(): Int {
@@ -29,6 +45,36 @@ class InterworkAccountAdapter(private val accountList: List<InterworkAccount>) :
             itemBinding.tvNickname.text = interworkAccount.nickname
             itemBinding.tvCode.text = interworkAccount.code
             itemBinding.tvUnlink.text = interworkAccount.unlinked
+
+            itemBinding.tvUnlink.setOnClickListener {
+                showDialog(context)
+            }
         }
+    }
+
+    private fun showDialog(context:Context){
+        val dialogViewBinding = AccountLinkedDialogBinding.inflate(LayoutInflater.from(context))
+        val dialog= androidx.appcompat.app.AlertDialog.Builder(context).create()
+        dialog.setView(dialogViewBinding.root)
+
+        // dialog 배경 투명 처리
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        // 다이얼로그 크기 설정
+        dialog.window?.setLayout(
+            WindowManager.LayoutParams.WRAP_CONTENT,
+            WindowManager.LayoutParams.WRAP_CONTENT)
+        dialog.setCanceledOnTouchOutside(false)    // 다이얼로그 영역 밖 클릭 시, 다이얼 로그 삭제 금지
+        dialog.setCancelable(true)                 // 취소가 가능하도록 하는 코드
+
+        dialog.show()
+
+        dialogViewBinding.button.setOnClickListener {
+            dialog.dismiss()
+        }
+        dialogViewBinding.button13.setOnClickListener {
+            dialog.dismiss()
+        }
+
     }
 }
